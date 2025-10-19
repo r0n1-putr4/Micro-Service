0db6415a0219fd96fe8d5daf4d5df8b4340580be
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2025-10-19 17:30:51 +0700

    Update Code

--- ecommerce-db/customer-service/db.sql
+++ ecommerce-db/customer-service/db.sql
@@ -1,16 +1,17 @@
-CREATE TABLE tb_customers (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL UNIQUE,
-  phone VARCHAR(20),
-  address VARCHAR(255),
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+CREATE TABLE `tb_customers` (
+  `id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,  
+  `avatar` varchar(255) DEFAULT NULL
+  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-INSERT INTO tb_customers (name, email, phone, address)
+INSERT INTO tb_customers (name, email, phone, address,avatar)
 VALUES
-('Roni Putra', 'roni@example.com', '081234567890', 'Padang, Sumatera Barat'),
-('Siti Aminah', 'siti@example.com', '081298765432', 'Jakarta Selatan'),
-('Budi Santoso', 'budi@example.com', '085212345678', 'Bandung, Jawa Barat');
+('Roni Putra', 'roni@example.com', '081234567890', 'Padang, Sumatera Barat',''),
+('Siti Aminah', 'siti@example.com', '081298765432', 'Jakarta Selatan',''),
+('Budi Santoso', 'budi@example.com', '085212345678', 'Bandung, Jawa Barat','');
 
